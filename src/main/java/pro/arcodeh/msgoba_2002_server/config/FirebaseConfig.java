cb81bfac68268bbb79ea747adfbbf756edb8d675
Odeh Adejoh <stradox@gmail.com>
2025-08-22 00:00:58 +0100

    added firebase dependency and config

---
+++ src/main/java/pro/arcodeh/msgoba_2002_server/config/FirebaseConfig.java
@@ -0,0 +1,37 @@
+package pro.arcodeh.msgoba_2002_server.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+    private final String googleAcctKey;
+
+    public FirebaseConfig(@Value("${spring.GOOGLE_ACCOUNT_KEY}") String googleAcctKey) {
+        this.googleAcctKey = googleAcctKey;
+    }
+
+    @Bean
+    public FirebaseApp initializeFirebase() throws IOException {
+        ClassPathResource resource = new ClassPathResource(googleAcctKey);
+        InputStream serviceAccount = resource.getInputStream();
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if(FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
+    }
+}
