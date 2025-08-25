c058d4410774cd7d3c4d63fedb7a9f8f13c00201
Odeh Adejoh <stradox@gmail.com>
2025-08-25 22:41:05 +0100

    implemented return of random 3 questions on authenticated route, and all questions only on admin route

---
+++ src/main/java/pro/arcodeh/msgoba_2002_server/repositories/ProfileRepository.java
@@ -0,0 +1,10 @@
+package pro.arcodeh.msgoba_2002_server.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.arcodeh.msgoba_2002_server.models.Profile;
+
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<Profile, UUID> {
+
+}
